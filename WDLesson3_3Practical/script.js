8a85e7b389d4b4f4cf8bb8b1b3e56319688b7032
qinhanw@nycstudents.net <qinhanw@nycstudents.net>
2026-01-06 19:02:32 +0000

    ydhdh

--- WDLesson3_3Practical/script.js
+++ WDLesson3_3Practical/script.js
@@ -1,1 +1,20 @@
 /* Challenge 2: Create a function for each image that display a description of the picture into the div with an id of highlight*/
+function w3schools1(){
+    document.getElementById("highlight").innerHTML="SGA";
+}
+
+function w3schools2(){
+     document.getElementById("highlight").innerHTML="Yang";
+}
+
+function w3schools3(){
+     document.getElementById("highlight").innerHTML="Curry";
+}
+
+function tutorial1(){
+     document.getElementById("highlight").innerHTML="Jordan and kobe";
+}
+
+function tutorial2(){
+     document.getElementById("highlight").innerHTML="Lakers";
+}
