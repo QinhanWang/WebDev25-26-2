667ccf8e39606cac0c5e839d30b2a807a3cd3fb5
qinhanw@nycstudents.net <qinhanw@nycstudents.net>
2026-01-22 18:37:00 +0000

    fnrn

--- WDLesson3_6Practical/script.js
+++ WDLesson3_6Practical/script.js
@@ -3,9 +3,9 @@ function avg(){
          let g1 = parseFloat(document.getElementById("g1").value);
          let g2 = parseFloat(document.getElementById("g2").value);
          let g3 = parseFloat(document.getElementById("g3").value);
-         let op = document.getElementById("output");
+         let op1 = document.getElementById("output1");
          let avg = (g1 + g2 + g3)/3;
-         op.innerHTML = `The average of all 3 grades is ${avg}`;
+         op1.innerHTML = `The average of all 3 grades is ${avg}`;
 }
 
 // Challenge 4:  Create the function for the Slope program.  Refer to the resource link in the HTML for assistance.
@@ -14,16 +14,16 @@ function slope(){
          let y1 = parseFloat(document.getElementById("y1").value);
          let x2 = parseFloat(document.getElementById("x2").value);
          let y2 = parseFloat(document.getElementById("y2").value);
-         let opu = document.getElementById("out");
+         let op2 = document.getElementById("output2");
          let m = (y2 - y1)/(x2 - x1);
-         opu.innerHTML = `Slope between points (${x1},${y1}) and (${x2},${y2}) is ${m}`;
+         op2.innerHTML = `Slope between points (${x1},${y1}) and (${x2},${y2}) is ${m}`;
 }
 
 // Challenge 6:  Create the function for the BMI program.  Refer to the resource link in the HTML for assistance.
 function bml(){
          let H = parseFloat(document.getElementById("h").value);
          let W = parseFloat(document.getElementById("w").value);
-         let pu = document.getElementById("put");
+         let op3 = document.getElementById("output3");
          let bml = W / H*H;
-         pu.innerHTML = ` The BMI is ${bml} `;
+         op3.innerHTML = ` The BMI is ${bml} `;
 }
